db00da0cc3abb812e73c3fe1137e488e79370b95
Rafael Yukio Kanaoka <rkanaoka2@gmail.com>
2018-05-21 17:51:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
 
     <title>Album example for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="album.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.1/examples/album/album.css" rel="stylesheet">
   </head>
 
   <body>
